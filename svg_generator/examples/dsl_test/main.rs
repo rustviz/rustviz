ebb5da0fc45f2cb4b80eb9360f1cb42d4bb3b57c
Marcelo Almeida <marceloalmeida98@gmail.com>
2021-02-18 01:57:05 -0500

    new dsl test example

---
+++ svg_generator/examples/dsl_test/main.rs
@@ -0,0 +1,11 @@
+/* --- Variable Definitions ---
+Owner x{_, Copy},
+Owner y{_, Copy}
+*/
+fn main() {
+    let x = 5; // !{ Duplicate(None->x) }
+    let y = x; // !{ Duplicate(x->y) }
+} /* !{
+    GoOutOfScope(x),
+    GoOutOfScope(y)
+} */
