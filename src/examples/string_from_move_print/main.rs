087729ecfd765f11bdfac75f62612bd67277fee7
SenmiaoORZ <hymanzzs@umich.edu>
2023-06-13 00:06:07 -0400

    clean again for merge

--- src/examples/string_from_move_print/main.rs
+++ src/examples/string_from_move_print/main.rs
@@ -1,11 +1,11 @@
 /* --- BEGIN Variable Definitions ---
-Owner y;
-Function println!();
 Owner x;
+Owner y;
 Function String::from();
+Function println!()
 --- END Variable Definitions --- */
 fn main() {
-    let x = String::from("hello");
-    let y = x;
-    println!("{}", y);
-}
+    let x = String::from("hello"); // !{ Move(String::from()->x) }
+    let y = x; // !{ Move(x->y) }
+    println!("{}", y); // !{ PassByStaticReference(y->println!())  }
+} // !{ GoOutOfScope(x), GoOutOfScope(y) }
